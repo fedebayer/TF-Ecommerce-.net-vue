fbd27266fbc3629d5444b225699e4b259b607ccb
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-06-19 18:13:17 -0300

    Added backend/BackendMemberController

---
+++ ProjectTeamFour_Backend/Controllers/BackendMemberController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ProjectTeamFour_Backend.Filters.ManagerFilter;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+    [ResponseCache(NoStore = true)]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class BackendMemberController : BaseController
+    {
+        public IActionResult BackendMemberIndex()
+        {
+            return View();
+        }
+    }
+}
