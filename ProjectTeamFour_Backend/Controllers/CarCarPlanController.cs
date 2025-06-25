97196afee3d45b0faf897796144e2b1b328ad3ec
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-06-25 18:26:28 -0300

    Added backend/CarCarPlanController

---
+++ ProjectTeamFour_Backend/Controllers/CarCarPlanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+    [ResponseCache(NoStore = true)]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+
+    public class CarCarPlanController : BaseController
+    {
+      
+        public IActionResult CarCarPlanIndex()
+        {
+            return View();
+        }
+    }
+}
