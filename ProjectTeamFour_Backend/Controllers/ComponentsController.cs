b6c831e938629ea0ae6e661c2ec911b1862d8142
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-06-27 18:13:18 -0300

    Added backend/ComponentsController

---
+++ ProjectTeamFour_Backend/Controllers/ComponentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+   
+    public class ComponentsController : BaseController
+    {
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+        public IActionResult Cards()
+        {
+            return View();
+        }
+    }
+}
