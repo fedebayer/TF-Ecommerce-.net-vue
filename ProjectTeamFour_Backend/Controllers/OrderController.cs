a7415dbe2ec4b497a55e8c8be77aeb8eb117cd25
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-07-04 18:09:19 -0300

    Added backend/OrderController

---
+++ ProjectTeamFour_Backend/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+    [ResponseCache(NoStore = true)]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class OrderController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
