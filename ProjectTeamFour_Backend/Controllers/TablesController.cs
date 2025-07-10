3eb09284db38d178391ce847b8009ae977563b47
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-07-10 18:10:23 -0300

    Added backend/TablesController

---
+++ ProjectTeamFour_Backend/Controllers/TablesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+    
+    public class TablesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Tables()
+        {
+            return View();
+        }
+    }
+}
