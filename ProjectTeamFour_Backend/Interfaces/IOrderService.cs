c45eafc236e93afbc56eea0a1aa1827e63f61430
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-07-30 18:28:06 -0300

    Added backend/interfaces/IOrderService

---
+++ ProjectTeamFour_Backend/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectTeamFour_Backend.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<OrderViewModel.OrderListResult> GetAll();
+        Task<string> DeleteOrder(OrderViewModel.OrderSingleResult order);
+
+        Task<string> UpdateOrder(OrderViewModel.OrderSingleResult orderUpdate);
+    }
+}
