d8b30531f6a3f2a886804e2480f379838d5ecc69
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-07-31 18:24:02 -0300

    Added backend/interfaces/IProjectService

---
+++ ProjectTeamFour_Backend/Interfaces/IProjectService.cs
@@ -0,0 +1,48 @@
+﻿using ProjectTeamFour_Backend.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ProjectTeamFour_Backend.ViewModels.CarCarPlanViewModel;
+
+namespace ProjectTeamFour_Backend.Interfaces
+{
+    public interface IProjectService
+    {
+        /// <summary>
+        /// 取得所有提案
+        /// </summary>
+        /// <returns></returns>
+        Task<ProjectViewModel.ProjectListResult> GetAll();
+
+        /// <summary>
+        /// 依類別查詢提案
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ProjectViewModel.ProjectListResult> GetByCategory(ProjectViewModel.GetByCategoryRequest request);
+
+        /// <summary>
+        /// 依Id查詢商品
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<ProjectViewModel.ProjectSingleResult> GetById(ProjectViewModel.GetByIdRequest request);
+
+        ProjectViewModel.ProjectSingleResult GetProjectById(int ProjectId);
+        public List<ProjectFAQViewModel> ConvertProjectFAQList(string strQuestion, string strAnswer);
+        public MemberViewModel.MemberSingleResult GetCreatorInfo(int MemberId);
+
+        public List<SelectPlanViewModel> GetPlanCards(int ProjectId);
+
+        Task<ProjectViewModel.ProjectListResult> GetTotalSale();
+
+
+        Task<ProjectViewModel.ProjectListResult> GetWaitForPass();
+
+
+        public Task<string> EditWaitForPassProject(ProjectViewModel.ProjectSingleResult waitForPassProject);
+
+        Task<ProjectViewModel.ProjectListforChart> GetAllForCharts();
+    }
+}
