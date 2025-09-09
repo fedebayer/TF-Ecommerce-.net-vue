ff22339b1205d903ddb40bb9a509a2a986c9469f
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-09-09 18:17:44 -0300

    Added backend/services/memberService

---
+++ ProjectTeamFour_Backend/Services/MemberService.cs
@@ -0,0 +1,50 @@
+﻿using ProjectTeamFour_Backend.Interfaces;
+using ProjectTeamFour_Backend.Models;
+using ProjectTeamFour_Backend.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Services
+{
+    public class MemberService: IMemberService
+    {
+        private readonly IRepository _dbRepository;
+
+        public MemberService(IRepository repository)
+        {
+            _dbRepository = repository;
+        }
+
+        public Task<MemberViewModel.MemberListResult> GetAll()
+        {
+            return Task.Run(() =>
+            {
+                MemberViewModel.MemberListResult result = new MemberViewModel.MemberListResult();
+                result.MemberList = _dbRepository.GetAll<Member>().Select(
+                    m => new MemberViewModel.MemberSingleResult()
+                    {
+
+                        MemberAddress = m.MemberAddress,
+                        MemberBirth = m.MemberBirth.ToString("d"),
+                        MemberConEmail = m.MemberConEmail,
+                        MemberId = m.MemberId,
+                        MemberName = m.MemberName,
+                        MemberPhone = m.MemberPhone,
+                        MemberRegEmail = m.MemberRegEmail,
+                        MemberWebsite = m.MemberWebsite,
+                        AboutMe = m.AboutMe,
+                        Gender = m.Gender,
+                        ProfileImgUrl = m.ProfileImgUrl,
+                        MemberTeamName = m.MemberTeamName,
+
+                    }).ToList();
+                return result;
+            });
+           
+        }
+
+       
+    }
+}
