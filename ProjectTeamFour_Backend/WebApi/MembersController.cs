93a73bc6ae001fe9ee424b964a6910de9fcc9ecd
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2025-12-12 18:20:12 -0300

    Added backend/webApi/MembersController

---
+++ ProjectTeamFour_Backend/WebApi/MembersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProjectTeamFour_Backend.Context;
+using ProjectTeamFour_Backend.Interfaces;
+using ProjectTeamFour_Backend.Models;
+using ProjectTeamFour_Backend.ViewModels;
+
+namespace ProjectTeamFour_Backend.WebApi
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly IMemberService _memberService;
+        private readonly ILogger<MembersController> _logger;
+
+        public MembersController(IMemberService memberService, ILogger<MembersController> logger)
+        {
+            _memberService = memberService;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task <BaseModel.BaseResult<MemberViewModel.MemberListResult>> GetAll()
+        {
+
+            var result = new BaseModel.BaseResult<MemberViewModel.MemberListResult>();
+
+            _logger.LogWarning(2001, DateTime.Now.ToLongDateString() + "MembersController GetAll方法被呼叫");
+            try
+            {
+                result.Body = await _memberService.GetAll();
+                return result;
+            }
+            catch(Exception ex)
+            {
+                result.Msg = ex.Message;
+                result.IsSuccess = false;
+
+                return result;
+            }
+
+        }
+
+    }
+}
