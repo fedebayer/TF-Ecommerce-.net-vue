c3a8109380fa4de4f5db364aa083ca44a40bd09c
Fede Bayer <fbayerque@alumnos.exa.unicen.edu.ar>
2026-08-07 18:01:12 -0300

    Added front/Assets/Javascript/CarCarPlanPage/carcarplanindex.js

---
+++ ProjectTeamFour/Assets/Javascript/CarCarPlanPage/carcarplanindex.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+
+
+    $('.project-categories-wrap').slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        autoplay: false,
+        infinite: true,
+        arrows: true,
+        accessibility: true,
+        prevArrow: $(".prev"),
+        nextArrow: $(".next"),
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        }
+        ]
+    });
+
+
+});
+
+
+//車車搜尋
+document.querySelector("#button-input").addEventListener("click", function (e) {
+    //if (e.key === "Enter") {
+
+        var url = "/CarCarPlan/Search/";
+        var searchString = document.querySelector("#text-input").value;
+        //console.log(searchString);
+
+        window.location.href = url + searchString;
+    //}
+});
+
